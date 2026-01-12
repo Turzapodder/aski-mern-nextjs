cffbf51f97d9b76271428205a1f2222240d9457a
turjha.podder <turjha.podder@technonext.com>
2026-01-12 10:22:41 +0600

    shadcn component fix

--- frontend/src/app/user/calendar/page.tsx
+++ frontend/src/app/user/calendar/page.tsx
@@ -1,15 +1,12 @@
 import React, { Suspense } from "react";
-import { Calendar } from "@/components/ui/calendar";
 import { CalendarSkeleton } from "@/components/calendar/skeletons/calendar-skeleton";
+import { Calendar } from "@/components/calendar/calendar";
 
 export default function CalendarPage() {
   return (
     <Suspense fallback={<CalendarSkeleton />}>
       <div className="container mx-auto py-10 flex justify-center">
-        <Calendar
-          mode="single"
-          className="rounded-md border shadow"
-        />
+        <Calendar />
       </div>
     </Suspense>
   );
